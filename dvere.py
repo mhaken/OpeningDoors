a7fa682f8700794e72eb6def4c55994f9063e578
mhaken <mhaken@live.com>
2015-09-21 10:47:08 +0200

    version 2

--- dvere.py
+++ dvere.py
@@ -4,6 +4,7 @@ Turris
 from __future__ import print_function
 import sys
 import datetime
+import time
 
 from device import Device
 
@@ -13,7 +14,6 @@ if __name__ == "__main__":
     reader = device.gen_lines()
 
     dvere_open="[01664712] JA-81M SENSOR LB:0 ACT:1"
-    dvere_closed="[01664712] JA-81M SENSOR LB:0 ACT:0"
 
     device = Device(device=device_name)
     reader = device.gen_lines()
@@ -22,5 +22,5 @@ if __name__ == "__main__":
         
 	if line == dvere_open:
 		device.send_command("TX ENROLL:0 PGX:0 PGY:1 ALARM:0 BEEP:NONE")
-        elif line == dvere_closed:
-		device.send_command("TX ENROLL:0 PGX:0 PGY:0 ALARM:0 BEEP:NONE")
+        time.sleep(5)
+        device.send_command("TX ENROLL:0 PGX:0 PGY:0 ALARM:0 BEEP:NONE")
